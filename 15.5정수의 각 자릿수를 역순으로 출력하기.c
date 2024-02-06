dc9d908af163dbd661536c66b346543f95b7a03e
snowykte0426 <140694064+snowykte0426@users.noreply.github.com>
2024-02-06 21:23:15 +0900

    15.5정수의 각 자릿수를 역순으로 출력 문제 정답코드 추가

---
+++ 15.5정수의 각 자릿수를 역순으로 출력하기.c
@@ -0,0 +1,25 @@
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    int count;
+    scanf("%d", &num);
+    count = num % 10;
+    num /= 10;
+    printf("%d ", count);
+    count = num % 10;
+    num /= 10;
+    printf("%d ", count);
+    count = num % 10;
+    num /= 10;
+    printf("%d ", count);
+    count = num % 10;
+    num /= 10;
+    printf("%d ", count);
+    printf("%d", num);
+    return 0;
+
+}
+*/
