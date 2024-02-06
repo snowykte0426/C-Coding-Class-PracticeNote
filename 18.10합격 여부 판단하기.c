1d0b64de3b522e5a646b30243553a4eb9c07666f
snowykte0426 <140694064+snowykte0426@users.noreply.github.com>
2024-02-06 21:51:31 +0900

    18.9~18.10파일 추가

---
+++ 18.10합격 여부 판단하기.c
@@ -0,0 +1,21 @@
+/*
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h> 
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    if ((0 <= a) && (a <= 100) && (0 <= b) && (b <= 100) && (0 <= c) && (c <= 100) && (0 <= d) && (d <= 100)) {
+        if ((a + b + c + d) / 4 >= 85) {
+            printf("합격");
+        }
+        else {
+            printf("불합격");
+        }
+    }
+    else {
+        printf("잘못된 점수");
+    }
+    return 0;
+}
+*/
---
+++ 18.8else 사용하기.c
@@ -0,0 +1,18 @@
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    char c1 = NULL;
+    scanf("%c", &c1);
+    if (c1 == 'a')
+    {
+        printf("a입니다.");
+    }
+    else
+    {
+        printf("a가 아닙니다.");
+    }
+    return 0;
+}
+*/
