52d1772f3e189057e2c3fa043148bbe5ba6f8d8a
snowykte0426 <140694064+snowykte0426@users.noreply.github.com>
2024-02-06 23:12:33 +0900

    22.8~27.12 파일 추가

---
+++ 23.8비트 논리 연산자 사용하기.c
@@ -0,0 +1,13 @@
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	unsigned int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	printf("%u\n", num1 ^ num2);
+	printf("%u\n", num1 | num2);
+	printf("%u\n", num1 & num2);
+	printf("%u\n", ~num1);
+}
+*/
