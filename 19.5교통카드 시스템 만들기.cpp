a6ae47e6ea3e8800380d3d1cc2e15d0bc387a715
snowykte0426 <140694064+snowykte0426@users.noreply.github.com>
2024-02-06 22:47:14 +0900

    19.5교통카드 시스템 만들기 파일 추가

---
+++ 19.5교통카드 시스템 만들기.cpp
@@ -0,0 +1,18 @@
+/*
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int main()
+{
+    int balance = 10000;
+    int age;
+    scanf("%d", &age);
+    if (age >= 7 && age <= 12)
+        balance -= 450;
+    else if (age >= 13 && age <= 18)
+        balance -= 720;
+    else if (age >= 19)
+        balance -= 1200;
+    printf("%d\n", balance);
+    return 0;
+}
+*/
