c027442a736cbc56cdde9fd429a20a1b1b17d5ee
snowykte0426 <snowykte0426@naver.com>
2024-03-09 21:25:30 +0900

    28.11교통카드 잔액 출력하기.c  정

--- 28.11교통카드 잔액 출력하기.c
+++ 28.11교통카드 잔액 출력하기.c
@@ -1,15 +1,13 @@
 /*
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main()
-{
+int main() {
     int bl = 0;
     scanf("%d", &bl);
-    while (bl > 0)
-    {
+    while (bl > 0) {
         bl -= 1200;
         if (bl < 0)
-            break;
+        break;
         printf("%d\n", bl);
     }
 }
